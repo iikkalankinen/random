7edf806baf154e952577eec67276a868d5ea7099
iikkalankinen <14200216+iikkalankinen@users.noreply.github.com>
2023-10-08 15:12:38 +0300

    Update read-csv.py
    Switched delimiter to semicolon.

--- csv-test/read-csv.py
+++ csv-test/read-csv.py
@@ -2,6 +2,6 @@ import csv
 
 file_path = input("Enter path to a csv file: ")
 with open(file_path, 'r') as f:
-    f_reader = csv.reader(f)
+    f_reader = csv.reader(f, delimiter=";")
     for row in f_reader:
         print(row)
