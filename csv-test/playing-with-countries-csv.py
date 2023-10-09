17dcdb5e6df9631ba199bae70147e937fb584ab0
iikkalankinen <14200216+iikkalankinen@users.noreply.github.com>
2023-10-09 18:13:20 +0300

    Update playing-with-countries-csv.py
    - Moved the whole thing inside a function
    - Added while loop to ask user input
    - Added check for csv file path with a while loop
    - Added match case statement for different selections

--- csv-test/playing-with-countries-csv.py
+++ csv-test/playing-with-countries-csv.py
@@ -1,9 +1,29 @@
 import csv
 
-file_path = input("Enter path to the countries csv file: ")
-country = input("Enter a country to check: ")
-with open(file_path, 'r') as f:
-    f_reader = csv.reader(f, delimiter=";")
-    for row in f_reader:
-        if row[1]==country:
-            print(country + " is  on position " + row[0] + " on world countries list with the population of " + row[2] + ".")
+def main():
+    print("Let's start!\n")
+    file_path = ""
+    suffix = ".csv"
+    while (file_path.endswith(suffix) == False):
+        file_path = input("Enter path to the countries csv file: ")
+    selection = "c"
+    while (selection != "q"):
+        selection = input("Choose one\n a) Check a country's population\n q) Quit\nYour choise: ")
+        selection = selection.lower()
+        match selection:
+            case "a":
+                country = input("Enter a country to check: ")
+                with open(file_path, 'r') as f:
+                    f_reader = csv.reader(f, delimiter=";")
+                    for row in f_reader:
+                        if row[1]==country:
+                            print(country + " is  on position " + row[0] + " on world countries list with the population of " + row[2] + ".")
+            case "b":
+                print(selection + " doesn't yet do anything.")
+            case "q":
+                print("Exiting the program.")
+            case _:
+                print("Sorry, I didn't understand that, please try again.")
+
+if __name__ == "__main__":
+    main()
