3c64842ff8e365abbf6ac364224b84ad0fad2766
iikkalankinen <14200216+iikkalankinen@users.noreply.github.com>
2023-10-10 06:15:52 +0300

    Update playing-with-countries-csv.py
    - Moved the file path query to it's own function

--- csv-test/playing-with-countries-csv.py
+++ csv-test/playing-with-countries-csv.py
@@ -1,11 +1,15 @@
 import csv
 
-def main():
-    print("Let's start!\n")
+def get_file_path():
     file_path = ""
     suffix = ".csv"
     while (file_path.endswith(suffix) == False):
         file_path = input("Enter path to the countries csv file: ")
+    return file_path
+
+def main():
+    print("Let's start!\n")
+    csv_file = get_file_path()
     selection = "c"
     while (selection != "q"):
         selection = input("Choose one\n a) Check a country's population\n q) Quit\nYour choise: ")
@@ -13,7 +17,7 @@ def main():
         match selection:
             case "a":
                 country = input("Enter a country to check: ")
-                with open(file_path, 'r') as f:
+                with open(csv_file, 'r') as f:
                     f_reader = csv.reader(f, delimiter=";")
                     for row in f_reader:
                         if row[1]==country:
