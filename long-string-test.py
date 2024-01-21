16cea6b493b650ebb7322cbee9b69560f6c3955d
iikkalankinen <14200216+iikkalankinen@users.noreply.github.com>
2024-01-21 11:21:43 +0200

    Create long-string-test.py
    Long string can easily be splitted into an array. Trying to think what else they are good for.

---
+++ long-string-test.py
@@ -0,0 +1,13 @@
+# long string can easily be splitted into an array
+long_string = """Star Destoryer
+Star Wraith
+Champion Firebird
+Star Eagle
+DX Leopard
+Champion Gator
+DX Roc
+Champion Toro
+JK Aviar""".split("\n")
+
+for i in long_string:
+    print(i)
